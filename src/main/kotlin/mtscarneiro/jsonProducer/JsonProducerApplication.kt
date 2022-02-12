ba53cf58e2789b1dbe4d66dfc1b0deb2d2effae1
Matheus Cunha Carneiro <dev.mtscarneiro@gmail.com>
2022-02-12 17:37:54 -0400

    Dojo code project started

---
+++ src/main/kotlin/mtscarneiro/jsonProducer/JsonProducerApplication.kt
@@ -0,0 +1,11 @@
+package mtscarneiro.jsonProducer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JsonProducerApplication
+
+fun main(args: Array<String>) {
+	runApplication<JsonProducerApplication>(*args)
+}
---
+++ src/test/kotlin/mtscarneiro/jsonProducer/JsonProducerApplicationTests.kt
@@ -0,0 +1,13 @@
+package mtscarneiro.jsonProducer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JsonProducerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
